dbf9e964bef11ae7cbfa21835516c8f3e6478f8f
Hassan <salamokokok4@gmail.com>
2025-08-06 11:26:25 +0330

    inititalize project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+let elem = <h1>My Name Is Hassan</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(elem);
